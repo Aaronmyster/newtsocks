72d3b0dd5d4ffb79e59ed1a33223cdbfdb881b2b
Aaron <aaronmyster@gmail.com>
2014-08-11 20:48:05 -0500

    Fixed spacing on comments

--- src/reutersPrototype.py
+++ src/reutersPrototype.py
@@ -18,7 +18,7 @@ def main():
 	print 'DONE!'
 
 def readSearchPage(companyModel,newsSourceModel,url,page):
-	#Only go 5 pages in for now. I'm working on my chromebook...
+	# Only go 5 pages in for now. I'm working on my chromebook...
 	if page == 5:
 		return
 	
@@ -26,30 +26,30 @@ def readSearchPage(companyModel,newsSourceModel,url,page):
 
 	tree = html.fromstring(requests.get(url).text)
 
-	#Get the list of all article URL's
+	# Get the list of all article URL's
 	articleURLs = tree.xpath("//*[@class='searchHeadline']/a/@href")
 
-	#add every article on the search page to the database
+	# add every article on the search page to the database
 	for articleUrl in articleURLs:
 		addNewsArticle(companyModel,newsSourceModel,articleUrl)
 
-	#go to the next page, and do the same thing...
+	# go to the next page, and do the same thing...
 	nextURL = tree.xpath("//*[@class='next']/a/@href")[0]
 	readSearchPage(companyModel,newsSourceModel,baseURL+nextURL,page+1)
 
 def addNewsArticle(companyModel,newsSourceModel,url):
 	tree = html.fromstring(requests.get(url).text)
 	
-	#Need the article title
+	# Need the article title
 	title = tree.xpath('//h1/text()')[0]
 
-	#Need the text of the article
+	# Need the text of the article
 	textElements = tree.xpath("//*[@id='articleText']//*/text()")
 	text = ""
 	for t in textElements:
 		text += t
 
-	#Need the date field
+	# Need the date field
 	dateText = tree.xpath("//div[@id='articleInfo']//span[@class='timestamp']/text()")[0]
 	date = datetime.datetime.strptime(dateText[:-4], "%a %b %d, %Y %I:%M%p").date()
 
