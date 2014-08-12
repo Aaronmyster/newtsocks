0d540a5617df1d8a0a76248c83c3d16dd52c6578
Aaron <aaronmyster@gmail.com>
2014-08-11 20:43:19 -0500

    Moved requirements into src. Trying to get landscape to compile correctly

--- src/models.py
+++ src/models.py
@@ -3,33 +3,33 @@ from peewee import *
 db = SqliteDatabase('../newtsocks.db')
 
 class BaseModel(Model):
-	class Meta:
-		database = db
+    class Meta:
+        database = db
 
 class Company(BaseModel):
-	name = CharField()
-	ticker = CharField()
+    name = CharField()
+    ticker = CharField()
 
 class NewsSource(BaseModel):
-	name = CharField()	
-	url = CharField()
+    name = CharField()    
+    url = CharField()
 
 class News(BaseModel):
-	company = ForeignKeyField(Company, related_name='news_fromCompany')
-	newsSource = ForeignKeyField(Company, related_name='news_fromSource')
-	title = CharField()
-	text = CharField()
-	url = CharField()
-	date = DateTimeField()
+    company = ForeignKeyField(Company, related_name='news_fromCompany')
+    newsSource = ForeignKeyField(Company, related_name='news_fromSource')
+    title = CharField()
+    text = CharField()
+    url = CharField()
+    date = DateTimeField()
 
 class Price(BaseModel):
-	company = ForeignKeyField(Company, related_name='prices')
-	date = DateTimeField()
-	openPrice = DoubleField()
-	highPrice = DoubleField()
-	lowPrice = DoubleField()
-	closePrice = DoubleField()
-	volumePrice = DoubleField()
+    company = ForeignKeyField(Company, related_name='prices')
+    date = DateTimeField()
+    openPrice = DoubleField()
+    highPrice = DoubleField()
+    lowPrice = DoubleField()
+    closePrice = DoubleField()
+    volumePrice = DoubleField()
 
-	
-	
+    
+    
