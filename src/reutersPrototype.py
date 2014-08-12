eabbc8604e19765b39d27784bfff868d36716cf9
Aaron <aaronmyster@gmail.com>
2014-08-11 21:44:21 -0500

    Fixed some more spacing

--- src/reutersPrototype.py
+++ src/reutersPrototype.py
@@ -7,60 +7,61 @@ import sys
 baseURL = 'http://www.reuters.com'
 
 def main():
-	companies = Company.select()
-	ns = NewsSource.select().where(NewsSource.name=='Reuters').get()
+    companies = Company.select()
+    ns = NewsSource.select().where(NewsSource.name=='Reuters').get()
 
-	for c in companies:
-		print "Finding stories about "+c.name
-		firstURL = baseURL+'/search?blob="'+c.name.replace(' ','+')+'"'
-		readSearchPage(c,ns,firstURL,1)
+    for c in companies:
+        print "Finding stories about "+c.name
+        firstURL = baseURL+'/search?blob="'+c.name.replace(' ','+')+'"'
+        readSearchPage(c,ns,firstURL,1)
 
-	print 'DONE!'
+    print 'DONE!'
 
 def readSearchPage(companyModel,newsSourceModel,url,page):
-	# Only go 5 pages in for now. I'm working on my chromebook...
-	if page == 5:
-		return
-	
-	print 'Reading Page: {0}'.format(page)
+    # Only go 5 pages in for now. I'm working on my chromebook...
+    if page == 5:
+        return
+    
+    print 'Reading Page: {0}'.format(page)
 
-	tree = html.fromstring(requests.get(url).text)
+    tree = html.fromstring(requests.get(url).text)
 
-	# Get the list of all article URL's
-	articleURLs = tree.xpath("//*[@class='searchHeadline']/a/@href")
+    # Get the list of all article URL's
+    articleURLs = tree.xpath("//*[@class='searchHeadline']/a/@href")
 
-	# add every article on the search page to the database
-	for articleUrl in articleURLs:
-		addNewsArticle(companyModel,newsSourceModel,articleUrl)
+    # add every article on the search page to the database
+    for articleUrl in articleURLs:
+        addNewsArticle(companyModel,newsSourceModel,articleUrl)
 
-	# go to the next page, and do the same thing...
-	nextURL = tree.xpath("//*[@class='next']/a/@href")[0]
-	readSearchPage(companyModel,newsSourceModel,baseURL+nextURL,page+1)
+    # go to the next page, and do the same thing...
+    nextURL = tree.xpath("//*[@class='next']/a/@href")[0]
+    readSearchPage(companyModel,newsSourceModel,baseURL+nextURL,page+1)
 
 def addNewsArticle(companyModel,newsSourceModel,url):
-	tree = html.fromstring(requests.get(url).text)
-	
-	# Need the article title
-	title = tree.xpath('//h1/text()')[0]
+    tree = html.fromstring(requests.get(url).text)
+    
+    # Need the article title
+    title = tree.xpath('//h1/text()')[0]
 
-	# Need the text of the article
-	textElements = tree.xpath("//*[@id='articleText']//*/text()")
-	text = ""
-	for t in textElements:
-		text += t
+    # Need the text of the article
+    textElements = tree.xpath("//*[@id='articleText']//*/text()")
+    text = ""
+    for t in textElements:
+        text += t
 
-	# Need the date field
-	dateText = tree.xpath("//div[@id='articleInfo']//span[@class='timestamp']/text()")[0]
-	date = datetime.datetime.strptime(dateText[:-4], "%a %b %d, %Y %I:%M%p").date()
+    # Need the date field
+    dateText = tree.xpath("//div[@id='articleInfo']//span[@class='timestamp']/text()")[0]
+    date = datetime.datetime.strptime(dateText[:-4], "%a %b %d, %Y %I:%M%p").date()
 
-	News.create(company=companyModel,
-		newsSource=newsSourceModel,
-		title=title,
-		text=text,
-		url=url,
-		date = date
-	)
-	print "Added: "+title
+    News.create(
+        company=companyModel,
+        newsSource=newsSourceModel,
+        title=title,
+        text=text,
+        url=url,
+        date = date
+    )
+    print "Added: "+title
 
 if __name__ == '__main__':
     sys.exit(main())
